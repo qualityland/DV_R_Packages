01bcf7d48e6b2406a9629253f8f83012c3d0eca9
Stefan Schmidt <stefan-2.schmidt_ext@novartis.com>
2021-06-24 18:57:30 +0200

    Installation of tidyverse and devtools added.

--- dv-r-packages.R
+++ dv-r-packages.R
@@ -1,11 +1,10 @@
-library(devtools)
 
-# list of packages
+# list of packages to be installed
 raw <-  read.csv("data/package-names.csv", col.names = c("name"))
 pkg <- raw$name
 
 # Check if packages are not installed and assign the
-# names of the uninstalled packages to the variable new.pkg
+# names of the not yet packages to the variable new.pkg
 new.pkg <- pkg[!(pkg %in% installed.packages())]
 
 # if there are any packages in the list that aren't installed,
@@ -13,6 +12,10 @@ new.pkg <- pkg[!(pkg %in% installed.packages())]
 if (length(new.pkg)) {
   install.packages(new.pkg, repos = "https://cran.rstudio.com")
 }
+
+install.packages(c("tidyverse", "devtools"))
+
+library(devtools)
 
 devtools::install_github("stan-dev/cmdstanr")
 devtools::install_github("UUPharmacometrics/PsNR")
