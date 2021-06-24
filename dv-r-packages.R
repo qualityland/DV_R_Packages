b84fe8c567a355a636e675b050aab62e403865a9
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2021-06-24 14:09:30 +0200

    data and script added.

---
+++ dv-r-packages.R
@@ -0,0 +1,19 @@
+
+# list of packages
+raw <-  read.csv("data/package-names.csv", col.names = c("name"))
+pkg <- raw$name
+
+# Check if packages are not installed and assign the
+# names of the uninstalled packages to the variable new.pkg
+new.pkg <- pkg[!(pkg %in% installed.packages())]
+
+# if there are any packages in the list that aren't installed,
+# install them
+if (length(new.pkg)) {
+  install.packages(new.pkg, repos = "https://cran.rstudio.com")
+}
+
+
+ip <- installed.packages()
+dim(ip)
+write.csv(ip, "data/installed-packages.csv")
